d2d669128fa6b8fbf6f388acbecb86b647a86313
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2022-10-13 20:48:37 +0500

    10 misol

--- build_func10.py
+++ build_func10.py
@@ -1,2 +1,8 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func10
+
+
+x=8
+y=4
+answer=round((3*pow(y,1/2)+pow(x,2/3)),2)
+print(answer)
