dc278cd54026012c4ab7f3ac768e4cc7341d8dad
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2022-10-13 22:21:52 +0500

    savol

--- build_func10.py
+++ build_func10.py
@@ -1,8 +1,9 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func10
+from tkinter import Y
 
 
-x=8
-y=4
-answer=round((3*pow(y,1/2)+pow(x,2/3)),2)
-print(answer)
+def main(x,y):
+    answer=3*pow(y,1/2)+pow(x,2/3)
+    return round(answer,2)
+print(main(8,4))
